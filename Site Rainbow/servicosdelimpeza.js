13227df907f4ee11b6a63321e5fd2cb92a5ca78e
VMarega <vinimarega@gmail.com>
2023-02-03 15:32:47 -0300

    itens para orçamento

--- Site Rainbow/servicosdelimpeza.js
+++ Site Rainbow/servicosdelimpeza.js
@@ -1,5 +1,8 @@
 const selecionado = document.querySelectorAll("[data-seleciona]");
+const sofa = document.querySelector("#sofa");
+const sofa_radius = document.querySelector("#sofa_radius");
 
+console.log(sofa);
 selecionado.forEach((e)=>{e.addEventListener('change', ()=>{
     if (e.checked) {
         console.log("Checkbox is checked..");
@@ -7,12 +10,6 @@ selecionado.forEach((e)=>{e.addEventListener('change', ()=>{
         console.log("Checkbox is not checked..");
       }})
 });
-
-/*selecionado.addEventListener('change', ()=>{
-    if(selecionado.checked) {
-        console.log("checked");
-    } else {
-        console.log("notchecekd");
-    }
-});*/
-
+//sofa.addEventListener('click', ()=>{
+//  const sofa_values = window.open('/descontaminacao_profunda/sofa_values.html')
+//})
