d671afae2bb7a7fc090d4ae340582fb9ef44dbb8
VMarega <vinimarega@gmail.com>
2023-02-05 18:31:37 -0300

    calculando pacotes

--- Site Rainbow/servicosdelimpeza.js
+++ Site Rainbow/servicosdelimpeza.js
@@ -1,12 +1,52 @@
-const sofaSelect = document.querySelectorAll("[data-size]");
-const package = document.querySelector("[data-pack]")
+const itemSelected = document.querySelectorAll("[data-size]");
+const package = document.querySelector("[data-pack]");
+const servico = document.querySelectorAll(".servico");
+
+let precoFinal = 0;
+let item = 1;
+let halfItem = 0.5;
+let bigItem = 2;
 /*function dizClick(){
   console.log("clicado")
 }*/
 
 
-sofaSelect.forEach((elemento)=>{
+itemSelected.forEach((elemento)=>{
   elemento.addEventListener("click", (evento)=>{
-    package.value = parseInt(package.value) + 1
-  })
-});
+    if (elemento.value < 2){
+    package.value = parseInt(package.value) + item
+    }
+    else {
+      package.value = parseInt(package.value) + bigItem}
+    })
+  });
+
+  /*function adicionaLista(){
+    document.getElementById(".valor").append("Sofá adicionado;");
+  }*/
+
+  function calculaPreco (){
+  if(package.value <= 5){
+    precoFinal += 200;
+  } else if ( package.value === 6) {
+    precoFinal += 240;
+  } else if (package.value === 7){
+    precoFinal += 250;
+  } else if (package.value > 7 && package.value < 10){
+    precoFinal = 250 + ((package.value - 7)*35);
+  } else if (package.value === 10) {
+    precoFinal = 330;
+  } else if (package.value > 10 && package.value < 15){
+    precoFinal = 330 + ((package.value - 10)* 31.5);
+  } else if (package.value === 15) {
+    precoFinal = 460;
+  } else {
+    alert(`entre em contato`);
+  }
+  return precoFinal;
+};
+
+function enquadraPacote (){
+  package.value
+  calculaPreco;
+}
